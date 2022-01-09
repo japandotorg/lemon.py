7a9c6c12c5830f9c16bfb17b2c533c40eac0b1d6
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2022-01-09 18:59:28 +0530

    startup cli added

--- main.py
+++ main.py
@@ -1,100 +1,82 @@
 #!/usr/bin/env python
 
+import asyncio
 import os
-import tomlkit
 import logging
-import nextcord
+from asyncio import get_event_loop
+from traceback import format_exc
 
-from pathlib import Path
-from nextcord.ext.tasks import loop
-from database import database as botdb
-from nextcord.ext import commands, tasks
+import click
+from asyncpg import Pool, create_pool
 
-config_file = Path("config.toml")
+import database as db
+from core import NextcordExample
+from config import postgres_uri, token
 
-if not config_file.is_file():
-    raise FileNotFoundError(
-        "Cannot find the config file"
+log = logging.getLogger("nextcord.example.bot")
+
+def run():
+    os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+    os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
+    os.environ["JISHAKU_HIDE"] = "True"
+    os.environ["PYTHONIOENCODING"] = "UTF-8"
+    
+    loop = asyncio.get_event_loop()
+    
+    bot = NextcordExample(loop=loop)
+    bot.pool = loop.run_until_complete(db.create_pool(bot=bot, dsn=postgres_uri, loop=bot.loop))
+    
+    bot.run(token)
+    
+@click.group(invoke_without_command=True, options_metavar="[options]")
+@click.pass_context
+def main(ctx):
+    """
+    Launches the client
+    """
+    if ctx.invoked_subcommand is None:
+        run()
+        
+@main.command(short_help="initialises the databases for the bot", options_metavar="[options]")
+@click.option("-s", "--show", help="show the output", is_flag=True)
+@click.option("--start_bot", help="run bot after", is_flag=True)
+def init(show: bool, start_bot: bool):
+    _run = get_event_loop().run_until_complete
+    try:
+        pool: Pool = _run(create_pool(dsn=postgres_uri))
+    except:
+        click.echo(f"Could not create database connection.\n{format_exc()}", err=True)
+        return
+
+    files = (
+        "general.sql",
+        "users.sql",
+        "events.sql",
+        "stats.sql",
+        "indexes.sql",
     )
-    
-bot_config = tomlkit.parse(config_file.read_text())
+    for file in files:
+        with open("scripts/sql/" + file, encoding="utf8") as f:
+            read = f.read()
+            if show:
+                print(read)
+            try:
+                _run(pool.execute(read))
+            except Exception:
+                click.echo(f"Failed on file {file}.\n{format_exc()}", err=True)
+                return
 
-if os.environ.get("MY_DATABAsE"):
-    bot_config["bot"]["database_url"] = os.environ.get("MY_DATABASE")
+    log.info("Created tables.")
 
-logger = logging.getLogger("nextcord")
-logger.setLevel(bot_config["bot"]["logging_level"])
-handler = logging.FileHandler(filename="nextcord.log", encoding="utf-8", mode="w")
-handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
-)
-logger.addHandler(handler)
+    if run:
+        run()
 
-myIntents = nextcord.Intents.all()
 
-description = "nextcord template bot"
-
-bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or(*bot_config["bot"]["prefixes"]),
-    description=description,
-    case_insensitive=True,
-    allowed_mentions=nextcord.AllowedMentions(
-        everyone=False,
-        users=False,
-        roles=False
-    ),
-    intents = myIntents
-)
-bot.remove_command('help')
-
-@tasks.loop(seconds=10.0)
-@bot.event
-async def on_connect():
-    await bot.change_presence(
-        status=nextcord.Status.idle,
-        activity=nextcord.Game(
-            name="Starting up..."
-        )
-    )
-    
-@bot.event
-async def on_disconnect():
-    for cog in bot.cogs:
-        bot.remove_cog(cog[0])
-        
-@bot.event
-async def on_ready():
-    print("[ {bot.user.name} ] Logged into discord...")
-    
-    logger.log(logging.INFO, f"Logged in as {bot.user.name} [ {bot.user.id} ]")
-    
-    activity = "{}help".format(bot_config["bot"]["prefixes"][0])
-    
-    await bot.change_presence(
-        status=nextcord.Status.online,
-        activity=nextcord.game(name=activity)
-    )
-    
-    global loaded_cogs
-    global conn
-    conn = await botdb.init_dbconn(bot_config["bot"]["database_url"])
-    
-    logger.log(logging.INFO, f"{bot.user.name} Ready")
-    
-@bot.command()
-async def load(ctx, extension):
-    if (ctx.messsage.author.permissions_in(ctx.message.channel).manage_messages):
-        bot.load_extension(f'cogs.{extension}')
-        await ctx.send(f'Loaded cog `{extension}')
-        
-@bot.command()
-async def unload(ctx, extension):
-    if (ctx.message.author.permissions_in(ctx.message.channel).manage_messages):
-        bot.unload_extension(f'cogs.{extension}')
-        await ctx.send(f'Unloaded cog `{extension}')
-        
-for filename in os.listdir('cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
-        
-bot.run(bot_config["bot"]["token"])
+if __name__ == "__main__":
+    try:
+        import uvloop
+    except ModuleNotFoundError:
+        log.warning("uvloop is not installed")
+    else:
+        uvloop.install()
+    main()
